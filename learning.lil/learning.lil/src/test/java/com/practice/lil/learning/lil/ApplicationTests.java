bf9e02116fdb3ba157cf027404b0bcdb1bb3ddf4
Joshua Adetola <96537636+Wlmve@users.noreply.github.com>
2022-01-21 19:01:56 -0500

    Add files via upload

---
+++ learning.lil/learning.lil/src/test/java/com/practice/lil/learning/lil/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.lil.learning.lil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
